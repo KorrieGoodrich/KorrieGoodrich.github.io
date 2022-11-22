1db7a1a514557fbc33081cdb9eb57b1c8e243fb5
korrie <korriegoodrich@gmail.com>
2022-11-21 18:33:32 -0700

    A10
    Created gear.html
    Added visual elements to nav.

--- project/js/requestsent.js
+++
@@ -1,6 +0,0 @@
-// This code will tell users that their information has been submitted. (Whether or not this actually works.)
-
-function submitFunction() {
-    let text = "Your information has been submitted!";
-    alert(text)
-}
