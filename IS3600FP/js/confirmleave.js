f88a20c94c016afb730b1ee1e61fa99c26768c42
korrie <korriegoodrich@gmail.com>
2022-12-04 19:25:44 -0700

    IS3600FP
    The start of my final project

---
+++ IS3600FP/js/confirmleave.js
@@ -0,0 +1,6 @@
+// This code will alert the user that clicking on the link will open in a new tab.
+
+function myFunction() {
+    let text = "This link will open in a new tab.";
+    alert(text)
+}
