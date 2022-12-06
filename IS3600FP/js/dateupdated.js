02206d1afedab989a9cb9ad7ad510aec860f01c3
korrie <korriegoodrich@gmail.com>
2022-12-06 14:44:02 -0700

    IS3600 FP
    Final version of IS3600 final project.

--- IS3600FP/js/dateupdated.js
+++ IS3600FP/js/dateupdated.js
@@ -1,3 +1,3 @@
 // This code adds the current date and time to the footer of each page.
 
-document.getElementById("korriedate").innerHTML = "Page Updated " + document.lastModified;
+document.getElementById("dateupdated").innerHTML = "Page Updated " + document.lastModified;
